bb7c9606cc7bf2d64dc98da20854569018f29acc
Kristian Kiraly <kristian.kiraly@gmail.com>
2022-10-21 21:47:32 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # CustomList
 
-A description of this package.
+Creating an alternative to List that doesn't crash when using ScrollViewProxy three times in a row. It allows fully customized cell generation, adding swipe actions, and dragging to reorder
+
+Usage:
+
+```swift
+struct ContentView: View {
+    @State var testList = [...]
+    
+    var body: some View {
+        CustomList(list: $testList)  { list, item, index in
+            row(list: list, item: item, index: index)
+        }
+    }
+        
+    func row(list:[TestStruct], item:TestStruct, index:Int) -> some View {
+        Text(item)
+        .addButtonActions(leadingButtons: [], trailingButton: [.delete]) { button in
+            print(button)
+        }
+    }
+```
--- Sources/CustomList/CustomList.swift
+++ Sources/CustomList/CustomList.swift
@@ -1,6 +1,120 @@
-public struct CustomList {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct CustomList<T:CustomListCompatible, Content>: View where Content: View {
+    @Binding var list:[T]
+    var allowsReordering:Bool = true
+    var tappedRowForItem:(Binding<T>) -> () = {item in print("Editing \(item)")}
+    var leftLabelString:(T) -> String = {_ in return "Left"}
+    var rightLabelString:(T) -> String = {_ in return "Right"}
+    @ViewBuilder var rowBuilder:([T], T, Int) -> Content
+    @State private var useDefaultRowBuilder = false
+    @State private var draggedItem: T?
+    
+    public var body: some View {
+        ScrollView {
+            LazyVStack(spacing:0) {
+                ForEach(Array($list.enumerated()), id: \.offset) { index, $item in
+                    rowDecider(list: list, item: item, index: index)
+                        .onTapGesture {
+                            tappedRowForItem($item)
+                        }
+                        .if(allowsReordering) { view in
+                            view.onDrag {
+                                self.draggedItem = item
+                                return NSItemProvider(item: nil, typeIdentifier: T.dragIdentifier)
+                            }
+                            .onDrop(of: [.data], delegate: CustomListDropDelegate(item: item, items: $list, draggedItem: $draggedItem))
+                        }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func rowDecider(list:[T], item:T, index:Int) -> some View {
+        if useDefaultRowBuilder {
+            defaultRowBuilder(list: list, item: item, index: index)
+        } else {
+            rowBuilder(list, item, index)
+        }
+    }
+    
+    func defaultRowBuilder(list:[T], item:T, index:Int) -> some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color(uiColor: .systemBackground))
+            VStack(spacing: 0) {
+                HStack {
+                    Text(leftLabelString(item))
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(rightLabelString(item))
+                }
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+                
+                if index != list.count - 1 { //If index isn't at the end of the list
+                    Divider()
+                        .padding(.leading, 25)
+                }
+            }
+        }
+    }
+}
+
+extension CustomList where Content == EmptyView {
+    init(list:Binding<[T]>, tappedRowForItem:@escaping (Binding<T>) -> () = {_ in }, leftLabelString:@escaping (T) -> String = {_ in "Left"}, rightLabelString:@escaping (T) -> String = {_ in "Right"}, allowsReordering:Bool = true) {
+        self.init(list:list, allowsReordering:allowsReordering, tappedRowForItem: tappedRowForItem, leftLabelString: leftLabelString, rightLabelString: rightLabelString, rowBuilder: { _,_,_ in EmptyView() })
+        self.useDefaultRowBuilder = true
+    }
+}
+
+public protocol CustomListCompatible: Equatable {
+    static var dragIdentifier:String { get } //"get" is so it's read-only
+}
+
+struct CustomListDropDelegate<T: Equatable> : DropDelegate {
+    let item : T
+    @Binding var items : [T]
+    @Binding var draggedItem : T?
+
+    func performDrop(info: DropInfo) -> Bool {
+        return true
+    }
+
+    func dropEntered(info: DropInfo) {
+        guard let draggedItem = self.draggedItem else {
+            return
+        }
+
+        if draggedItem != item {
+            guard
+                let from = items.firstIndex(of: draggedItem),
+                let to = items.firstIndex(of: item) else {
+                return
+            }
+            withAnimation(.default) {
+                self.items.move(fromOffsets: IndexSet(integer: from), toOffset: to > from ? to + 1 : to)
+            }
+        }
+    }
+    
+    func dropUpdated(info: DropInfo) -> DropProposal? {
+        return DropProposal(operation: .move)
+    }
+}
+
+private extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
     }
 }
